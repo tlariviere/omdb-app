95ad404ab14f508fed7df1903a62cf98340e13e0
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 12:05:33 +0100

    Implement MovieContent component to display movie details

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,1 +1,25 @@
 export type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export interface Movie {
+  Actors: string;
+  Awards: string;
+  BoxOffice: string;
+  Country: string;
+  Director: string;
+  Genre: string;
+  Language: string;
+  Metascore: string;
+  Plot: string;
+  Poster: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Title: string;
+  Type: string;
+  Website: string;
+  Writer: string;
+  Year: string;
+  imdbID: string;
+  imdbRating: string;
+  imdbVotes: string;
+}
