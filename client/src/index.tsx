dbe7c574c6c603bad4ea143b6f254af826e511b2
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 12:02:54 +0100

    Setup react query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./containers/App";
+import queryDefaultOptions from "./constants/queryDefaultOptions";
 import "./styles/globals.scss";
+
+const queryClient = new QueryClient({
+  defaultOptions: queryDefaultOptions,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
