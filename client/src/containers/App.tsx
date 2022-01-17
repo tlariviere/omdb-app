4de6b7dfb52766edf513ed1f8e8f5d5f242a6595
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 11:17:03 +0100

    Setup client app router
    - /movie/:id => display movie detailed info given its imdb ID.
    - /search[?title=<title>[&page=<page>]] => search movie database by
    title.
    - / => redirect to /search.

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -6,10 +7,21 @@ import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
-    <div className={styles.App}>
-      <Header />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className={styles.App}>
+        <Header />
+
+        <main>
+          <Routes>
+            <Route path="/movie/:imdbId" element={<div />} />
+            <Route path="/search" element={<div />} />
+            <Route path="*" element={<Navigate to="/search" replace />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
