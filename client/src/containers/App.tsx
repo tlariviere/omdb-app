83eedda55d869af599bf576fae8e2feeb5469b2b
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 11:15:20 +0100

    Implement footer

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
     <div className={styles.App}>
       <Header />
+      <Footer />
     </div>
   );
 };
