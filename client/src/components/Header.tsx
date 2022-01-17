dec6b9a84794b6c5bd1015ae34877cf86c98afc8
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 11:13:54 +0100

    Implement header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../assets/logo.png";
+import styles from "./Header.module.scss";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.Header}>
+      <nav>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Header from "../components/Header";
 import styles from "./App.module.scss";
 
-const App: React.FC = () => <div className={styles.App} />;
+const App: React.FC = () => {
+  return (
+    <div className={styles.App}>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
