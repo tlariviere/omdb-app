a105d1f9ee9971f1ed5a8c2025a0c1b6210e9620
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 11:22:18 +0100

    Serve client app static files in production mode

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
@@ -6,6 +7,16 @@ import config from "./constants/server";
 const app = express();
 app.use(logger("dev"));
 app.use(helmet());
+
+if (process.env.NODE_ENV === "production") {
+  const pages = ["/movie/:token", "/search"];
+  app.get(pages, (req, res, next) => {
+    req.url = "/"; // react-router requires to serve index.html on any valid page route.
+    next();
+  });
+
+  app.get("/*", express.static(path.join(__dirname, "public")));
+}
 
 app.listen(config.SERVER_PORT, () =>
   console.log(`Server running at http://127.0.0.1:${config.SERVER_PORT}/`)
