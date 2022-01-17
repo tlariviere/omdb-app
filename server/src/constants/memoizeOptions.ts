1949919bac0094b40f15a1b68b1c71c08ac20985
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2022-01-17 11:41:07 +0100

    Memoize omdb query results up to 24 hours

---
+++ server/src/constants/memoizeOptions.ts
@@ -0,0 +1,7 @@
+const memoizeOptions = {
+  primitive: true,
+  promise: true,
+  maxAge: 86400000, // 24h
+};
+
+export default memoizeOptions;
--- server/src/controllers/fetchMovie.ts
+++ server/src/controllers/fetchMovie.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
+import memoize from "memoizee";
 
 import type { OmdbRes } from "../utils/types";
 import omdbConfig from "../constants/omdb";
+import memoizeOptions from "../constants/memoizeOptions";
 import handleOmdbQuery from "../utils/handleOmdbQuery";
 
-const fetchMovieData = async (imdbId: string) => {
+const memoizedFetchMovie = memoize(async (imdbId: string) => {
   const { data } = await axios.get<OmdbRes>("http://www.omdbapi.com/", {
     params: { apikey: omdbConfig.OMDB_API_KEY, i: imdbId },
   });
   return data;
-};
+}, memoizeOptions);
 
 const fetchMovie = async (imdbId: string) =>
-  handleOmdbQuery(fetchMovieData(imdbId));
+  handleOmdbQuery(memoizedFetchMovie(imdbId));
 
 export default fetchMovie;
